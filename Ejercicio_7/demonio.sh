8b93b574a41ed1063489696a37588da0699d4295
Sebastian Emanuel Enrique Bogado <seebogado@hotmail.com>
2015-11-26 20:40:51 +0000

    Update demonio.sh

--- Ejercicio_7/demonio.sh
+++ Ejercicio_7/demonio.sh
@@ -1,112 +1,197 @@
 #!/bin/bash
 
-BackUp()
-{
-	fecha_actual=$(date + "%d-%m-%y")
-	Archivo_a_guardar="$path_destino$fecha_actual.tar.gz"
-	tar -zcvf $Archivo_a_guardar $path_destino
+#####################################################################################################
+ 
+ayuda() {
+     
+	echo "Los comandos aceptados para el manejo del demonio son:"
+	echo "  start: Inicia la ejecución del demonio. Se debe ingresar "
+	echo "por parámetros el directorio al que se le desea realizar el/los"
+	echo " backup/s, el directorio donde desea guardar los mismos y el "
+	echo "intervalo de tiempo entre backups expresado en segundos."
+	echo "  Stop: Finalizar el demonio."
+	echo "  count: Indica la cantidad de archivos de backup que hay "
+	echo "en el directorio"
+	echo "  clear: Limpia el directorio de backup, recibe por parametro"
+	echo "la cantidad de backup que mantiene la carpeta, siendo estos "
+	echo "los últimos generados. Ningún parametro equivale a cero."
+	echo "  play: El demonio crea el backup en ese instante."
+	echo ""
+	echo "Ejemplo de ejecucion comando Start:"
+	echo  "bash ./Ejercicio7.sh Opcion Path-Origen Path-Destino Intervalo"
+	echo  "bash ./Ejercicio7.sh start /root/Desktop/Origen/ /root/Desktop/Destino/ 5"
+	echo "Nota:Solo se puede ejecutar un demonio al mismo tiempo."
+	echo "Segunda Nota: El archivo demonio.pid se debe encontrar en el path /tmp/"	
+}
+ 
+
+#####################################################################################################
+
+function CheckearDemonio {     #Verifico si el demonio fue instanciado o no
+    if [ -f $TEMP ]     #Si existe el archivo temporal con el pid del demonio
+    then
+        DEM=`ps -fea |grep $( head -1 $TEMP ) |grep -v grep -c`  #tomo el pid
+        if [ $DEM = 0 ]             #verifico si sigue en ejecucion el demonio
+        then               
+            rm $TEMP $PARAMETROS    #borro los archivos temporales que no se pudieron borrar en la ejecucion pasada
+	#rm /tmp/datostemporales.txt
+            (( IsReady= 1 ))    #Guardo el resultado en la variable IsReady
+        else
+            (( IsReady= 0 ))
+        fi
+    else
+        (( IsReady= 1 ))        #si no existe el archivo $TEMP entonces el demonio no esta en ejecucion seguramente
+    fi
 }
 
-play()
+#####################################################################################################
+
+func_start()
 {
-	echo "haciendo backup instantaneo de $path_origen a $path_destino"
-	BackUp
+	CheckearDemonio
+	
+	if [ $IsReady -eq 0 ]
+	then
+		echo "El demonio ya se encuetra iniciado"
+		exit 1
+	else
+		path_origen=$1
+		path_destino=$2
+		intervalo=$3
+		#if [ ! -e /tmp/datostemporales.txt ]
+		if [  -e /tmp/datostemporales.txt ]
+		then
+			rm /tmp/datostemporales.txt
+		fi
+		#si no existe el archivo significa que no se ejecuto entonces lo ejecuto
+		if [ ! -f "/var/run/demonio.pid" ]; then
+			#creo canal fifo de comunicacion
+			touch /tmp/datostemporales.txt
+			chmod +rwx /tmp/datostemporales.txt
+			#$path_origen = "$1"
+			#$path_destino="$2"
+			#$intervalo=$3
+			echo "$path_origen" >> /tmp/datostemporales.txt
+			echo "$path_destino" >> /tmp/datostemporales.txt
+			echo "$intervalo" >> /tmp/datostemporales.txt
+			echo "Ejecutando Damonio"
+			nohup /tmp/demonio.sh&
+			#bg
+			pid_hijo=$!
+			#Cuando creo el proceso , creo el canal de comunicacion que contiene el pid de ejecucion
+		else
+			echo "El proceso esta corriendo...Recuperando PID del demonio"
+			#como esta ejecutado busco su pid
+			pid_hijo=$(`cat /tmp/demonio.pid`)
+		fi
+		#echo "el pid delproceso es " $pid_hijo
+		#wait $pid_hijo
+	fi
+	
 }
 
-count()
+#####################################################################################################
+
+func_stop()
 {
-	read path_origen < /tmp/demfifo
-	#Lista los archivos y los pasa a la tuberia al comando wc para contarlos
-	Cantidad_de_archivos_backup=$(find $path_origen -type f -name "*.sh" | wc -l)
-	echo "$Cantidad_de_archivos_backup" > /tmp/demfifo
+    CheckearDemonio   #Verifico el estado del demonio
+    if [ $IsReady -eq 0 ]
+    then
+        echo "Finalizando proceso..."
+	#señal de STOP (19)
+	kill -SIGTERM $pid_hijo
+    else
+        echo "El demonio no fue instanciado"
+        exit 1
+    fi
+
 }
+
+#####################################################################################################
+
+func_count()
+{
+	CheckearDemonio   #Verifico el estado del demonio
+
+	if [ $IsReady -eq 0 ]
+	then
+		echo "$1" > /tmp/demfifo
+		kill -SIGUSR1 $pid_hijo
+		sleep 1
+		read cantidad < /tmp/demfifo
+		echo "la cantidad es: $cantidad"
+		if [ -f "/tmp/demfifo" ]
+		then
+			rm /tmp/demfifo
+		fi
+	else
+		echo "El demonio no fue instanciado"
+		exit 1
+	fi
+
+
+}
+
+#####################################################################################################
+
+func_play()
+{
+    CheckearDemonio   #Verifico el estado del demonio
+    if [ $IsReady -eq 0 ]
+    then
+        echo "iniciando backup instantaneo"
+	kill -SIGUSR2 $pid_hijo
+    else
+        echo "El demonio no fue instanciado"
+        exit 1
+    fi
+
+}
+
+#####################################################################################################
 
 func_clear()
 {
-	echo "Iniciando la funcion clear."
-	echo "Borrando archivos de $path_destino"
-	find $path_destino -name '*.sh'  -exec rm -f {} \;
+    CheckearDemonio   #Verifico el estado del demonio
+    if [ $IsReady -eq 0 ]
+    then
+       	echo "limpiando carpeta"
+	kill -SIGALRM $pid_hijo
+    else
+        echo "El demonio no fue instanciado"
+        exit 1
+    fi
 }
 
-iteracion()
-{
-	sleep $interval &
-	wait $!
-}
+#####################################################################################################
 
-salir()
-{
-	if [ -e /tmp/demonio.pid ]
-	then
-		# El fichero PID existe
-		if [ `cat /tmp/demonio.pid` -eq $$ ]
-		then
-			# El PID nos corresponde
-			echo "borrando el fichero PID, porque es de esta ejecucion."
-			rm -f /tmp/demonio.pid
-		fi
-	fi
-
-	rm /tmp/demfifo
-	exit 0
-}
-
-trap salir SIGTERM SIGKILL
-trap count SIGUSR1
-trap play SIGUSR2 
-trap func_clear SIGALRM
-
-
-echo "Iniciando demonio..."
-
-### VERIFICO SI EL DEMONIO YA ESTA EN EJECUCION:
-if [ ! -e /tmp/demonio.pid ]
+TEMP="/tmp/demonio.pid"
+#si el demonio esta en ejecucion , cargo su PID
+if [ -e /tmp/demonio.pid ]
 then
-	# no existe el fichero PID
-	echo $$ > /tmp/demonio.pid
-else
- 	# El fichero PID existe
-	if [ `cat /tmp/demonio.pid` -ne $$ ]
-	then
-		echo "Imposible ejecutar, este binario ya se está ejecutando."
-		salir
-	fi
+	pid_hijo=`cat /tmp/demonio.pid`
 fi
-###
-
-#recibo los parametros
-read path_origen < "/tmp/datostemporales.txt"
-path_destino=$(head -2 "/tmp/datostemporales.txt" | tail -1)
-#intervalo de ejecucion de los backup
-intervalo=$(head -3 "/tmp/datostemporales.txt" | tail -1)
-#intervalo que uso con los sleeps
-interval=1
-#borro el archivo que uso como canal
-rm "/tmp/datostemporales.txt"
-
-#echo "El path origen es:$path_origen"
-#echo "El path destino es: $path_destino"
-#variable que marca el intervalo de ejecucion del demonio
-#echo "el intevalo es: $interval"
-
-
-echo "arrancando el demonio con PID=$$"
-
-while true
-do
-	# hacer lo que sea
-	echo "iteracion"
-	# esperar
-	echo "esperando $interval segundos"
-	iteracion
-	
-	if [ $i -eq $intervalo ]
-	then
-		i=0
-		play
-	fi
-	i=$((i+1))
-
-done
-
-##fin de demonio
+case "$1" in
+	start)
+		func_start $2 $3 $4
+	;;
+	stop)
+		#Envio señal para que finalice el proceso
+		func_stop
+	;;
+	count)
+		func_count $2
+	;;
+	clear)
+		func_clear $2
+	;;
+	play)
+		func_play
+	;;
+	help)
+		ayuda
+	;;
+#opcion por default
+*)
+ayuda
+esac
